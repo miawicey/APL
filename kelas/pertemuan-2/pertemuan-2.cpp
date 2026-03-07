dc8fdda1501920e1a0ca18142ec4012ce21540ff
miawicey <vitakhairana@gmail.com>
2026-03-08 06:18:37 +0800

    pertemuan2

---
+++ kelas/pertemuan-2/pertemuan-2.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+// int bilangan[5]; // Array integer, 5 elemen
+// string nama[4]; // Array string, 4 elemen
+// float nilai[10]; // Array float, 10 elemen
+// // Deklarasi + Inisialisasi langsung
+// string buah[4] = {"Jeruk", "Mangga", "Nanas", "Apel"};
+// //Deklarasi array tanpa inisialisasi elemen array
+
+// buah[4] = "Rambutan";
+// for (int i = 0; i < 5; i ++) {
+// cout << buah[i] << endl; }
+// // sebut dulu nama array nya baru elemen nya ?
+
+// int main(){
+// // int panjang= 0
+// //  string mahasiswa[MAX_MAHASISWA];
+// // int pilihan, index;
+
+// // do{
+// //     cout <<"\n======MAHASISWA======" << endl;
+// //     cout << "1. Tampilkan Mhs" << endl;
+// //     cout <<"2. Tambah mhs" << endl;
+// //     cout << "3. Ubah Mhs" << endl;
+// //     cout <<"4. Hapus Mhs" << endl;
+// //     cout << "5. Keluar" << endl;
+// //     cout <<"Pilihan;" << endl;
+// //     cin >> pilihan ;
+
+
+
+// // int nilai[2][2][2]= {
+// //    { {80,96}, {75,90}}, 
+// //    {{88,92}, {70,75}} }
+// //    ;
+
+// //    cout << "=====DATA MAHASISWA=====\N\N";
+
+// //    for (int m=0; m<2;m++) {
+// //         cout << " Mahasiwa" << m+1 << endl;
+// //         cout << " UTS : " << nilai [k][m][0] <<  endl;
+// //         cout << "uas :"<< nilai [k][m][1]<< endl;
+// //    }
+// //    cout << endl;
+
+// //    }
+
+// //    return 0;
+// // }
+// struct alamat {
+//     string jalan;
+//     int nomor;
+//     string kota;
+
+// };
+
+// struct mhs {
+//     int umur;
+//     string nama;
+//     string jurusan ;
+//     alamat almt;
+// };
+
+
+// mhs Mhs;
+// Mhs.jurusan= "informatika;
+// Mhs.nama="ARUSHA";
+// Mhs.almt.jalan= "Jln wijaya kusumma";
+// Mhs.almt.nomor=22;
+// Mhs.almt.kota= "Samarinda"
+
+// }
+
+// struct date {
+//     int month;
+//     int year;
+//     int day;
+// };
+
+// struct mahasiswa {
+//     string name;
+//     struct date birth;
+// };
+
+
+// struct mahasiswa mhs[10];
+// mhs[0].name= "ihsan";
+// mhs
+
+
+
+
+
